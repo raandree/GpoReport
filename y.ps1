6eb17750a97eecabf2217dc2b27d77b0135b0530
Raimund Andree <r.andree@live.com>
2025-06-04 10:49:50 +0200

    wip

--- y.ps1
+++ y.ps1
@@ -1,6 +1,39 @@
-$searchValue = 'Account Lockout'
-$xmlPath = 'C:\Git\GpoReport\reports\t1.xml'
+function ConvertTo-GpoObject {
+    param (
+        [Parameter(Mandatory = $true)]
+        [object]$Object
+    )
+
+    $result = [ordered]@{}
+    $properties = $Object | Get-Member -MemberType Property
+    foreach ($property in $properties) {
+        if ($property.Definition -like '*[[]]*') {
+            
+            foreach ($item in $element.($property.Name)) {
+                if ($item -is [System.Xml.XmlElement]) {
+                    $result[$property.Name] += @(ConvertTo-GpoObject -Object $item)
+                }
+                else {
+                    $result[$property.Name] += @($item)
+                }
+            }
+        }
+        else {
+            if ($property.Definition -like 'System.Xml.XmlElement*') {
+                $result.Add($property.Name, (ConvertTo-GpoObject -Object $Object.($property.Name)))
+            }
+            else {
+                $result.Add($property.Name, $Object.($property.Name))
+            }
+        }
+    }
+    [PSCustomObject]$result
+}
+
+$searchValue = ''
+$xmlPath = 'D:\Git\GpoReport\reports\t2.xml'
 $xml = [xml](Get-Content $xmlPath)
+$propertiesToSkip = 'Explain'
 
 # Register the namespace used in the XML
 $xmlNameSpaceNodes = $xml.SelectNodes('//namespace::*[not(. = ../../namespace::*)]')
@@ -20,8 +53,26 @@ foreach ($nsNode in $xmlNameSpaceNodes) {
     }
 }
 
-$elements = foreach ($xmlNamespace in $xmlNameSpaces.GetEnumerator()) {
-    $xml.SelectNodes("//*[namespace-uri() = '$($xmlNamespace.Value)']", $xmlNameSpaceManager)
+#$elements = foreach ($xmlNamespace in $xmlNameSpaces.GetEnumerator()) {
+#    $xml.SelectNodes("//*[namespace-uri() = '$($xmlNamespace.Value)']", $xmlNameSpaceManager) |
+#        ForEach-Object {
+#            if( -not ($_.ChildNodes.Count -eq 1 -and $_.ChildNodes[0].Name -eq '#text') )
+#            {
+#                $_
+#            }
+#            else
+#            {
+#                Write-Host 'x'
+#            }
+#        }
+#}
+
+$elements = @()
+$elements += foreach ($extension in $xml.GPO.Computer.ExtensionData) {
+    $extension.Extension.ChildNodes
+}
+$elements += foreach ($extension in $xml.GPO.User.ExtensionData) {
+    $extension.Extension.ChildNodes
 }
 
 if ($searchValue) {
@@ -31,55 +82,36 @@ if ($searchValue) {
 #$possibleParentNodeTypes = 'Policy', 'UserRightsAssignment', 'SecurityOptions'
 Write-Host "Found $($elements.Count) elements containing '$searchValue':" -ForegroundColor Green
 foreach ($element in $elements) {
-    Write-Host "- $($element.InnerText)"
+    Write-Host "- $element"
 
-    $parentNode = $element.ParentNode
+    $elementTypeName = $element.psobject.TypeNames[0].Split('#')[-1]
 
-    $elementTypeName = $parentNode.psobject.TypeNames[0].Split('#')[-1]
-    if ($elementTypeName -notin $possibleParentNodeTypes) {
-        continue
-    }
-
-    $properties = @{}
-    foreach ($property in ($parentNode | Get-Member -MemberType Property)) {
-        $properties[$property.Name] = $parentNode.($property.Name)
-    }
-    [PSCustomObject]$properties
-
-    <#
-    switch ($elementTypeName)
-    {
-        { $_ -eq 'Policy' }
-        {
-            [PSCustomObject]@{
-                Name      = $parentNode.Name
-                State     = $parentNode.State
-                Explain   = $parentNode.Explain
-                Supported = $parentNode.Supported
-                Category  = $parentNode.Category
-                Comment   = $parentNode.Comment
+    $result = [ordered]@{}
+    foreach ($property in ($element | Get-Member -MemberType Property)) {
+        if ($propertiesToSkip -contains $property.Name) {
+            continue
+        }
+        
+        if ($property.Definition -like '*[[]]*') {
+            
+            foreach ($item in $element.($property.Name)) {
+                if ($item -is [System.Xml.XmlElement]) {
+                    $result[$property.Name] += @(ConvertTo-GpoObject -Object $item)
+                }
+                else {
+                    $result[$property.Name] += @($item)
+                }
             }
         }
-        { $_ -eq 'UserRightsAssignment' }
-        {
-            [PSCustomObject]@{
-                Name      = $parentNode.Name
-                State     = $parentNode.State
-                Explain   = $parentNode.Explain
-                Supported = $parentNode.Supported
-                Comment   = $parentNode.Comment
+        else {
+            if ($property.Definition -like 'System.Xml.XmlElement*') {
+                $result[$property.Name] = ConvertTo-GpoObject -Object $element.($property.Name)
             }
-        }
-        { $_ -eq 'SecurityOption' }
-        {
-            [PSCustomObject]@{
-                Name      = $parentNode.Name
-                State     = $parentNode.State
-                Explain   = $parentNode.Explain
-                Supported = $parentNode.Supported
-                Category  = $parentNode.Category
+            else {
+                $result[$property.Name] = $element.($property.Name)
             }
         }
     }
-    #>
+    [PSCustomObject]$result
+
 }
